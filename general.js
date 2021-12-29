8a2430b815c4221137c8fc781ee150357d98a7b3
Divashna Kumar <divashna7@gmail.com>
2021-12-29 16:39:03 +1100

    Aesthetic Updates
    Update background and main headline.

--- general.js
+++ general.js
@@ -1,4 +1,8 @@
 // Smooth scrolling
 function smoothScroll() {
     document.documentElement.style.scrollBehavior = "smooth";
-}
+};
+
+if (document.getElementsByClassName("mainHeading").length < 100 ) {
+    document.getElementById("headline")
+};
